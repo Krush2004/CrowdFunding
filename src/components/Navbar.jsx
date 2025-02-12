bf32f0fe59592b4c153fc9717e58fbad83e4fa00
Amandeep <a2guggi11052002@gmail.com>
2025-02-13 02:19:16 +0530

    darkmode issue solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,10 @@ const Navbar = ({setSearchQuery, userCampaigns, theme, activeState}) => {
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div id='input' className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px]
        bg-[#1c1c24] rounded-[100px]">
-        <input type="text"
+        <input type="text" id='search-input'
         onChange={(e)=> setSearchQuery(e.target.value)}
          placeholder="Search for campaigns"
-         className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
+         className={`flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] ${isLight? 'text-black' : 'text-white'} bg-transparent outline-none`} />
         
         <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
           <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
