625281f8f02c6ea61f4888e8616dfaa1a5c451de
Amandeep <a2guggi11052002@gmail.com>
2025-02-13 11:20:39 +0530

    new client and secret key added

--- src/client.jsx
+++ src/client.jsx
@@ -2,7 +2,11 @@ import { createThirdwebClient } from "thirdweb";
 import React from "react";
 
 
+// export const client = createThirdwebClient({
+//   clientId: 'f969692f0d51bcb4b9c0b0542ab59f15',
+//   secretKey: 'j1ruUjECF1I8I5XqbNEdVwstvHkjDgu3lHX2skDuMCDNKPLxuSQJD_Aj-RVOElTQodIzu6FvZjgLwvwb3525gQ'
+// });
 export const client = createThirdwebClient({
-  clientId: 'f969692f0d51bcb4b9c0b0542ab59f15',
-  secretKey: 'j1ruUjECF1I8I5XqbNEdVwstvHkjDgu3lHX2skDuMCDNKPLxuSQJD_Aj-RVOElTQodIzu6FvZjgLwvwb3525gQ'
+  clientId: '5f0e8315b495c25f8e7669dc4753da69',
+  secretKey: 'J8pP6ip35CNxp9Un0CDI55v8hizPwyNnHap4wHe0UODWdcuRMIJqGrwLxpWTaKp0VA8gk8L8DG0F0wpL-bOWnA'
 });
