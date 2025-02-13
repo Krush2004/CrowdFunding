56ed51beed90e346eb410ab6b89dc058d0e4788b
Amandeep <a2guggi11052002@gmail.com>
2025-02-13 10:46:20 +0530

    client.js file added

---
+++ src/client.jsx
@@ -0,0 +1,8 @@
+import { createThirdwebClient } from "thirdweb";
+import React from "react";
+
+
+export const client = createThirdwebClient({
+  clientId: 'f969692f0d51bcb4b9c0b0542ab59f15',
+  secretKey: 'j1ruUjECF1I8I5XqbNEdVwstvHkjDgu3lHX2skDuMCDNKPLxuSQJD_Aj-RVOElTQodIzu6FvZjgLwvwb3525gQ'
+});
