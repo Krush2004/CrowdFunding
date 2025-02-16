c4c60dde1cb1b42ad9b6297f1e43228ab0399411
anirudha <anirudhakolay08062001@gmail.com>
2025-02-16 21:34:00 +0530

    changes anirudh name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = ({setSearchQuery, userCampaigns, theme, activeState}) => {
           styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
           handleClick={() => {
             if(address) {
-              navigate('create-campaign')
+              navigate('/create-campaign')
               setIsActive('campaign')
             }
             else {
